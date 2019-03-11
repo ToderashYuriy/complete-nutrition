942bed8388e1fe0a2aa22d0f07ca5bfc628c2487
Yura Toderash <t0derash12345@gmail.com>
2019-03-11 18:00:09 +0200

    Fix Video Block in Product

--- src/assets/custom.js
+++ src/assets/custom.js
@@ -70,9 +70,51 @@ $(document).ready(function(){
 	});
 });
 
-
-
-
+$('a.magnific_popup_items.product-single__photo-zoom.js-no-transition').each(function(i){
+	var that = $(this),
+		alt = that.data('alt');
+	if(alt.indexOf('youtube') >= 0 ){
+		that.addClass('video_product_zoom');
+		that.attr('href', alt);
+		$('.video_product_zoom').magnificPopup({
+		  type: 'iframe',
+		  iframe: {
+		    markup: '<div class="mfp-iframe-scaler">'+
+		              '<div class="mfp-close"></div>'+
+		              '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		            '</div>',
+		    patterns: {
+		      youtube: {
+		        index: 'youtube.com/',
+		        id: 'v=',
+		        src: '//www.youtube.com/embed/%id%?autoplay=1'
+		      }
+		    },
+		    srcAction: 'iframe_src',
+		  }
+		});
+	}else{
+		that.addClass('js-product-zoom');
+		$(".js-product-zoom").magnificPopup({
+		    type: "image",
+		    image: {
+		      verticalFit: false
+		    },
+		    gallery: {
+		      enabled: true,
+		      arrowMarkup:
+		        '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"><i class="icon icon--%dir% mfp-prevent-close"></i></button>'
+		    },
+		    mainClass: "product-single-zoom mfp-medium mfp-zoom-pic mfp-close-corner",
+		    fixedContentPos: true,
+		    removalDelay: 200,
+		    closeOnContentClick: false,
+		    autoFocusLast: false,
+		    closeMarkup:
+		      '<button title="Close (Esc)" type="button" class="mfp-close mfp-close--custom js-close-mfp"><i class="icon icon--close"></i></button>'
+		});
+	}
+});
 
 
 
