eba8322b416c7a53078e2059164bbace33d6651b
Yura Toderash <t0derash12345@gmail.com>
2019-03-23 19:52:47 +0200

    Filter

---
+++ src/assets/bc-sf-search.js
@@ -0,0 +1,13 @@
+// Override Settings
+var bcSfSearchSettings = {
+    search: {
+        //suggestionMode: 'test',
+        //suggestionPosition: 'right',
+        //suggestionWidth: 'auto'
+    }
+};
+
+// Customize style of Suggestion box
+BCSfFilter.prototype.customizeSuggestion = function(suggestionElement, searchElement, searchBoxId) {
+  	
+};
